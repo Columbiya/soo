1f6969de8325f1202056a5e6608eed6a5fdbf923
Columbiya <volodiabykov216@gmail.com>
2023-10-26 09:54:15 +0500

    init

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,11 @@
-import { ButtonHTMLAttributes, FC } from "react";
+import { ButtonSize, ButtonType } from '@/types'
+import { ButtonHTMLAttributes, FC } from 'react'
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+	buttonType: ButtonType
+	buttonSize: ButtonSize
+}
 
 export const Button: FC<ButtonProps> = ({ children, ...props }) => {
-    return (
-        <button {...props}>{children}</button>
-    )
-};
+	return <button {...props}>{children}</button>
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,15 @@
 export const enum Constants {
     PROJECT_NAME = 'Сервис общественных обсуждений'
 }
+
+export const enum ButtonType {
+    Green,
+    Blue,
+    OutlinedBlue,
+    White
+}
+
+export const enum ButtonSize {
+    Default,
+    Small
+}
