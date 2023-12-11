68d3447edc935507966e92fbdeda6d8781eec281
Columbiya <volodiabykov216@gmail.com>
2023-12-11 17:31:45 +0500

    fix pagination

--- src/components/project-page/ProjectsList/index.tsx
+++ src/components/project-page/ProjectsList/index.tsx
@@ -78,7 +78,6 @@ export const ProjectsList: FC<ProjectsListProps> = ({ items, totalCount }) => {
 						count={pagesCount}
 						page={page}
 						onChange={onPageChange}
-						hidePrevButton={page === 1}
 						hideNextButton={page === pagesCount}
 						color="primary"
 						shape="rounded"
